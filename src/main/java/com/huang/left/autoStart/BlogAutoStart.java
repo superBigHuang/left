53cb5b2429a29dd1d22609ce5f63640bce983edb
superBigHuang <841069102@qq.com>
2020-10-11 10:15:14 +0800

    项目一启动就执行blogRedis中的一些方法

---
+++ src/main/java/com/huang/left/autoStart/BlogAutoStart.java
@@ -0,0 +1,26 @@
+package com.huang.left.autoStart;
+
+import com.huang.left.controller.BlogController;
+import com.huang.left.entity.Blog;
+import com.huang.left.redis.BlogRedisDao;
+import com.huang.left.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BlogAutoStart implements CommandLineRunner {
+
+    @Autowired
+    private BlogRedisDao blogRedisDao;
+    @Autowired
+    private BlogService blogService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Blog> all = blogService.findAll();
+        blogRedisDao.saveListAllBLog(all);
+    }
+}
