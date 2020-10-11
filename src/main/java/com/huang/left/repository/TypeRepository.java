051c8c56ef1c3747bc54fb77a05f83c7978405ba
superBigHuang <841069102@qq.com>
2020-10-11 09:57:05 +0800

    新增根据用户id查询Type

--- src/main/java/com/huang/left/repository/TypeRepository.java
+++ src/main/java/com/huang/left/repository/TypeRepository.java
@@ -4,7 +4,12 @@ import com.huang.left.entity.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface TypeRepository extends JpaRepository<Type,Long>, JpaSpecificationExecutor<Type> {
     // 根据名称查找
     Type findByTypeName(String typeName);
+
+    // 根据用户id查询关注的类型
+    List<Type> findByUser_Id(Long id);
 }
